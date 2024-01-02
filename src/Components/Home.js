33b7ca0a7cfc9a4650b25bbfddcd2bad177947b8
unniimohan <myprojectsunni@gmail.com>
2024-01-02 13:42:11 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Components/Home.css'
+import logo from '../images/logo.png'
+
+function Home() {
+  return (
+   <>
+    <div id='homeContent_1'>
+        <img id='logo' src={logo} alt="" />
+    </div>
+
+   </>
+  )
+}
+
+export default Home
